8c6202a955fd01f3e729dc739232c1f0f4c434dd
Benjamin M Lancaster <bmlancaster07@gmail.com>
2019-02-05 23:27:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,71 +1,20 @@
-class Shelter   #have many dogs 
-  attr_accessor :name, :city
+class Dog
+  attr_accessor :name
+
   @@all = []
 
-  def initialize (name, city)
+  def initialize(name)
     @name = name
-    @city = city
-    @dogs = [] 
-    @@all << self 
-  end 
-  
-  def add_dog(dog)
-    dog.shelter = self
-    @dogs << dog 
-  end 
-  
-  def dogs 
-    @dogs 
-  end 
-  
-  def self.all 
-    @@all
-  end 
-end 
-
-class Dog  #belong to a shelter & an owner
-  attr_accessor :name, :age, :breed, :shelter, :owner
-  @@all = []
-  
-  def initialize(name, age, breed)
-    @name = name
-    @age = age
-    @breed = breed 
-    @@all << self 
-  end 
-  
-  def self.all
-    @@all
-  end 
-end 
-
-
-class Owner # has many dogs 
-  attr_accessor :name, :age
-  attr_reader :dogs 
-  @@all = []
-  
-   def initialize (name, age)
-    @name = name
-    @age = age
-    @dogs = []
-    @shelters = []
-    @@all << self 
-  end 
-  
-  def add_dog(dog)
-    @dogs << dog 
-    dog.owner = self 
-  end 
-  
-  def shelters
-    self.dogs.each do |dog| 
-      @shelters << dog.shelter if dog.shelter
-    end 
-    @shelters
+    @@all << self
   end
-  
-  def self.all 
-    @@all
-  end 
-end
+
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+  end
+
+  def self.clear_all
+    @@all.clear
+  end
+end 
